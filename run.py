90547e07f89626700a2025c8a601bb458f2ed06a
SakshiKekre <sakshi.kekre@gmail.com>
2024-05-06 22:08:15 -0700

    Updates for CORS

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Admin1234@database-1.c7sweeci81lq.us-east-1.rds.amazonaws.com:3306/final_project'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
